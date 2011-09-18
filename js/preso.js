588cb82586a14c2f38e04b92ec5e137085d5d9c9
hoorayimhelping <bucky@buckyschwarz.net>
2011-09-18 00:36:05 -0400

    Started cleaning up CSS.

--- js/preso.js
+++ js/preso.js
@@ -1,40 +1,40 @@
 $(function() {
-	$('#prev').live('click', function() {
-		return navigate('prev');
-	});
+    $('#prev').live('click', function() {
+        return navigate('prev');
+    });
 
-	$('#next').live('click', function () {
-		return navigate('next');
-	});
+    $('#next').live('click', function () {
+        return navigate('next');
+    });
 });
 
 var navigate = function(direction) {
-	var url = document.URL,
-		chunks = url.split('/'),
-		page = chunks[chunks.length - 1],
-		currentPage = 0;
+    var url = document.URL,
+        chunks = url.split('/'),
+        page = chunks[chunks.length - 1],
+        currentPage = 0;
 
-	if (direction === 'prev') {
-		if (page !== 'index.html') {
-			currentPage = parseInt(page.split('.')[0], 10);
+    if (direction === 'prev') {
+        if (page !== 'index.html') {
+            currentPage = parseInt(page.split('.')[0], 10);
 
-			if (currentPage === 1) {
-				currentPage = 'index';
-			} else {
-				currentPage--;
-			}
-		} else {
-			return false;
-		}
-	}
+            if (currentPage === 1) {
+                currentPage = 'index';
+            } else {
+                currentPage--;
+            }
+        } else {
+            return false;
+        }
+    }
 
-	if (direction === 'next') {
-		if (page !== 'index.html') {
-			currentPage = parseInt(page.split('.')[0], 10);
-		}
-		currentPage++;
-	}
-	console.log(currentPage);
-	window.location.href = currentPage + '.html';
-	return false;
+    if (direction === 'next') {
+        if (page !== 'index.html') {
+            currentPage = parseInt(page.split('.')[0], 10);
+        }
+        currentPage++;
+    }
+    console.log(currentPage);
+    window.location.href = currentPage + '.html';
+    return false;
 }
